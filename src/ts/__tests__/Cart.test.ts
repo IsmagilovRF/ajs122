7a028a6384e36cfad1d9cbead33afaa3425fd39c
IsmagilovRF <ledohod@icloud.com>
2021-02-04 19:59:08 +0300

    edit fun

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -31,3 +31,29 @@ test('добавление элемента Movie в корзину', () => {
   expect(cart.items.length).toBe(1);
 });
 
+
+test('подсчет суммарной стоимости Book + MusicAlbum + Movie', () => {
+  const cart = new Cart();
+  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+  cart.add(new Movie(1011, 'The Avengers', 1500, 2012, 'США', 'Avengers Assemble!', 'фантастика, боевик', '137 мин'));
+
+  expect(cart.totalCost()).toBe(4400);
+});
+
+test('подсчет суммарной стоимости с учетом скидки', () => {
+  const cart = new Cart();
+  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+
+  expect(cart.discountCost(10)).toBe(2610);
+});
+
+test('удаление элемента из корзины', () => {
+  const cart = new Cart();
+
+  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+  cart.remove(1001);
+
+  expect(cart.items.length).toBe(0);
+});
--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -2,12 +2,40 @@ import Buyable from '../domain/Buyable';
 
 export default class Cart {
     private _items: Buyable[] = [];
-
+  
     add(item: Buyable): void {
-        this._items.push(item);
+      this._items.push(item);
     }
-
+  
     get items(): Buyable[] {
-        return [...this._items]; 
+      return [...this._items];
     }
-}
+  
+    totalCost(): number {
+      let totalCost = 0;
+      this._items.forEach(element => {
+        totalCost += element.price;
+      });
+  
+      return totalCost;
+    }
+  
+    discountCost(discount: number): number {
+      let totalCost = 0;
+      this._items.forEach(element => {
+        totalCost += element.price;
+      });
+  
+      return totalCost - totalCost / 100 * discount;
+    }
+  
+    remove(id: number): void {
+      const i: number = this._items.findIndex(element => {
+        return element.id === id;
+      });
+  
+      if (i >= 0) {
+        this._items.splice(i, 1);
+      }
+    }
+  }
